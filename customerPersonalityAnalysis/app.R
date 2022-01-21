a020f5bb78660d8a465065be0c71d7655aa30ed1
Lim Tik Hong <s2110684@siswa.um.edu.my>
2022-01-21 15:08:32 +0800

    Data Cleaning, EDA and Feature Engeneering

--- customerPersonalityAnalysis/app.R
+++ customerPersonalityAnalysis/app.R
@@ -13,36 +13,71 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+    titlePanel("Customer Personality Analysis"),
+    
+    fluidRow(
+      column(3,
+        wellPanel(
+          h4("Filter"),
+          sliderInput("year_birth",
+                      "Customer's birth year",
+                      1893,1996,c(1900,1950)),
+          selectInput("education", "Education",
+                      c("Graduation", "PhD", "Master", "Basic", "2n cycle")
+          ),
+          checkboxGroupInput("status", "Marital Status",
+                             c("Single", "Together", "Married", "Divorced", "Widow", "Alone")
+          ),
+          selectInput("spendType", "Amount spent on",
+                      c("Wine", "Fruit", "Meat", "Fish", "Sweets", "Gold")
+          ),
+          sliderInput("income",
+                      "Total of yearly household income",
+                      1730,666666,c(3000,50000)),
+          sliderInput("deals",
+                      "Number of purchases made with a discount",
+                      0,15,1),
+          sliderInput("website",
+                      "Number of purchases made through the company’s website",
+                      0,27,1),
+          sliderInput("catalogue",
+                      "Number of purchases made using a catalogue",
+                      0,28,1),
+          sliderInput("stores",
+                      "Number of purchases made directly in stores",
+                      0,13,1),
+          sliderInput("visits",
+                      "Number of visits to company’s website in the last month",
+                      0,20,1),
         )
-    )
+      ),
+      column(9,
+             sidebarLayout(
+               sidebarPanel(
+                 sliderInput("bins",
+                             "Number of bins:",
+                             min = 1,
+                             max = 50,
+                             value = 30)
+               ),
+               
+               # Show a plot of the generated distribution
+               mainPanel(
+                 plotOutput("distPlot")
+               )
+             )
+      )
+    ),
+    #wellPanel(
+    #  selectInput("xvar", "X-axis variable", axis_vars, selected = ""),
+    #  selectInput("yvar", "Y-axis variable", axis_vars, selected = "Amount spent on wine"),
+    #)
+    
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
 }
 
 # Run the application 
--- rCode.R
+++ rCode.R
@@ -1,2 +1,36 @@
-link <- "https://raw.githubusercontent.com/UM-S2110684/customerPersonalityAnalysis/master/marketing_campaign.csv"
-dataset <- read.csv(link,sep = '\t')
+#Read data set
+df <- read.csv("marketing_campaign.csv",sep = '\t')
+
+#Data Cleaning Process
+#Remove "Z_CostContact" and "Z_Revenue" that non-informative
+df <- subset(df, select = -c(Z_CostContact, Z_Revenue))
+#Feature Engeneering
+#Encode "Education" and "Marital_Status" into numeric forms
+table(df$Marital_Status)
+table(df$Education)
+df["Marital_Status"][df["Marital_Status"] == 'Alone'] <- 'Single'
+df["Marital_Status"][df["Marital_Status"] == 'YOLO'] <- 'Single'
+df["Marital_Status"][df["Marital_Status"] == 'Absurd'] <- 'Single'
+
+# calculate the age instead of 'Year-Birth'
+df['Year_Birth']= 2022-df['Year_Birth']
+names(df)[names(df)=="Year_Birth"] <- "Age"
+#Total spending on all items
+df['Total_Spent'] = df["MntWines"]+ df["MntFruits"]+ df["MntMeatProducts"]
++ df["MntFishProducts"]+ df["MntSweetProducts"]+ df["MntGoldProds"]
+
+# creating a new feature indicates the number of days customer engaged to the company
+df$Dt_Customer <- as.Date(df$Dt_Customer, format="%d-%m-%Y")
+newest_customer <- max(df$Dt_Customer)
+df['newest_customer'] = newest_customer
+df['days_engaged'] <- (df['newest_customer'] - df['Dt_Customer'])
+df <- subset(df, select = -c(Dt_Customer, newest_customer))
+
+df <- na.omit(df)
+df <- subset(df, Age<100 & Income<600000)
+
+df['Education_encode'] <- as.numeric(as.factor(df$Education))
+df['Marital_Status_encode'] <- as.numeric(as.factor(df$Marital_Status))
+df_enc <- subset(df, select=c(Education_encode, Education))
+df_enc1 <- subset(df, select=c(Marital_Status_encode, Marital_Status))
+df <- subset(df, select = -c(Education, Marital_Status))
